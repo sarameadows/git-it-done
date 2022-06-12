a132d71b3ca35ff01f1ca1591c252db78af59c3e
Sara <sameads9@gmail.com>
2022-06-12 18:14:44 -0500

    link to repository if has over 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,18 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,19 +23,23 @@ var getRepoIssues = function(repo) {
                 response.json().then(function(data) {
                     //pass response data to dom function
                     displayIssues(data);
+                    //check if api has paginated issues
+                    if(response.headers.get("link")) {
+                        displayWarning(repo);
+                    }
                 });
             } else {
                 alert("There was a problem with your request!");
             }
         });
-}
+};
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
         return;
     }
-    
+
     for(var i = 0; i < issues.length; i++) {
         //create a link element to take users to the issue on github
         var issueEl = document.createElement("a");
@@ -48,6 +66,6 @@ var displayIssues = function(issues) {
 
         issueContainerEl.appendChild(issueEl);
     }
-}
+};
 
 getRepoIssues("facebook/react");
